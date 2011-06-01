857ba86875adda08772b98375c92545f0256b13f
Wondong LEE <wdlee91@gmail.com>
2011-06-01 15:24:15 +0000

    힘든 주석 작업

--- common/src/catdog/mine/Inventory.java
+++ common/src/catdog/mine/Inventory.java
@@ -4,13 +4,28 @@ import java.util.ArrayList;
 
 public class Inventory
 {
+	/**
+	 * 인벤토리에 들어있는 아이템 목록. 비어있는 칸은 <code>null</code>.
+	 */
 	private ArrayList<Item> itemList;
+	
+	/**
+	 * 인벤토리에 들어있는 아이템의 수
+	 */
 	private int items;
 	
-	private static final int INVENTORY_WIDTH = 5;
-	private static final int INVENTORY_HEIGHT = 3;
-	
-	private static final int MAX_ITEMS = INVENTORY_WIDTH * INVENTORY_HEIGHT;
+	/**
+	 * 인벤토리의 횡폭
+	 */
+	public static final int INVENTORY_WIDTH = 5;
+	/**
+	 * 인벤토리의 종폭
+	 */
+	public static final int INVENTORY_HEIGHT = 3;
+	/**
+	 * 인벤토리의 실제 크기
+	 */
+	public static final int MAX_ITEMS = INVENTORY_WIDTH * INVENTORY_HEIGHT;
 	
 	public Inventory()
 	{
@@ -18,6 +33,11 @@ public class Inventory
 		items = 0;
 	}
 	
+	/**
+	 * 인벤토리 비어있는 칸에 아이템을 추가한다.
+	 * @param item 아이템 객체
+	 * @return 성공 여부
+	 */
 	public boolean addItem(Item item)
 	{
 		for(int i = 0; i < MAX_ITEMS; ++ i)
@@ -33,6 +53,11 @@ public class Inventory
 		return false;
 	}
 	
+	/**
+	 * 해당하는 인벤토리 칸의 아이템을 제거한다.
+	 * @param index 인벤토리 번호
+	 * @return 성공 여부
+	 */
 	public boolean removeItem(int index)
 	{
 		if(itemList.get(index) != null)
@@ -44,11 +69,20 @@ public class Inventory
 		return false;
 	}
 	
+	/**
+	 * 해당하는 인벤토리 칸의 아이템 객체를 가져온다.
+	 * @param index 인벤토리 번호.
+	 * @return 해당하는 아이템 객체. 해당 칸이 비어있다면 <code>null</code>.
+	 */
 	public Item getItem(int index)
 	{
 		return itemList.get(index);
 	}
 	
+	/**
+	 * 인벤토리에 들어있는 아이템 수를 얻는다.
+	 * @return 아이템 수
+	 */
 	public int getAvailableItems()
 	{
 		return items;
--- common/src/catdog/mine/ItemDB.java
+++ common/src/catdog/mine/ItemDB.java
@@ -4,20 +4,34 @@ import java.util.*;
 
 public class ItemDB
 {
+	/**
+	 * 아이템 사전
+	 */
 	private static TreeMap<Integer, ItemProperty> itemDic;
 	
 	static
 	{
 		itemDic = new TreeMap<Integer, ItemProperty>();
 		
+		// 아이템 ID와 구체적인 아이템 속성을 연결
 		itemDic.put(1, new ItemProperty("Item 1", "data/item1.png"));
 	}
 	
+	/**
+	 * <code>itemID</code>에 해당하는 아이템 속성을 얻어옴
+	 * @param itemID 아이템 ID
+	 * @return 아이템의 속성
+	 */
 	public static ItemProperty getItemProperty(int itemID)
 	{
 		return itemDic.get(itemID);
 	}
 	
+	/**
+	 * 이름이 <code>itemName</code>인 아이템의 ID를 얻어옴
+	 * @param itemName 아이템 이름
+	 * @return 아이템 ID. 해당 이름의 아이템이 없다면 -1
+	 */
 	public static int getItemID(String itemName)
 	{
 		Set<Map.Entry<Integer, ItemProperty>> items = itemDic.entrySet();
