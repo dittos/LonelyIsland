5f96079f07a87b0cff06c07c81516675b47f3877
Taeho Kim <dittos@gmail.com>
2011-06-07 16:58:48 +0000

    * 게임 오버 스크린 예쁘게 바꿈 * 게임 오버 후 재시작 가능하게 함

--- common/src/catdog/mine/GameOverScreen.java
+++ common/src/catdog/mine/GameOverScreen.java
@@ -2,13 +2,42 @@ package catdog.mine;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameOverScreen implements Screen {
+	
+	private BitmapFont font;
+	private SpriteBatch spriteBatch;
+	private int days;
+	private Game game;
+	
+	public GameOverScreen(Game game, int days) {
+		this.game = game;
+		this.days = days;
+	}
 
 	@Override
 	public void render(float delta) {
+		if (Gdx.input.justTouched()) {
+			game.setScreen(new GameScreen(game));
+			return;
+		}
+		
 		Gdx.gl10.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		spriteBatch.begin();
+		font.setColor(Color.RED);
+		printCenter("GAME OVER", 250);
+		font.setColor(Color.WHITE);
+		printCenter("You've survived for " + days + " days", 200);
+		printCenter("Touch the screen to retry...", 80);
+		spriteBatch.end();
+	}
+	
+	private void printCenter(String str, float y) {
+		font.draw(spriteBatch, str, (Gdx.graphics.getWidth() - font.getBounds(str).width) / 2, y);
 	}
 
 	@Override
@@ -19,8 +48,8 @@ public class GameOverScreen implements Screen {
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-
+		font = new BitmapFont();
+		spriteBatch = new SpriteBatch();
 	}
 
 	@Override
--- common/src/catdog/mine/GameScreen.java
+++ common/src/catdog/mine/GameScreen.java
@@ -3,6 +3,7 @@ package catdog.mine;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -18,17 +19,46 @@ public class GameScreen implements Screen {
 	private SpriteBatch spriteBatch;
 	private Game game;
 	
+	private static final float WHITEOUT_TIME = 1.5f;
+	private float whiteout = WHITEOUT_TIME;
+	private Texture whiteTex;
+	
 	public GameScreen(Game game) {
 		this.game = game;
 	}
 
 	@Override
 	public void render(float delta) {
-		if (GameState.gameOver) {
-			game.setScreen(new GameOverScreen());
-			return;
+		if (!GameState.gameOver) {
+			update(delta);
+		} else {
+			if (whiteout <= 0) {
+				game.setScreen(new GameOverScreen(game, Clock.getDay()));
+				return;
+			}
 		}
 		
+		Gdx.gl10.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		world.render(viewport);
+		player.render(viewport);
+		if (!GameState.gameOver) {
+			inventoryView.render();
+			if (craftDialog.shown)
+				craftDialog.render();
+			
+			spriteBatch.begin();
+			font.draw(spriteBatch, String.format("Day %d - %s", Clock.getDay(), Clock.isNight()? "Night" : "Day"), 10, 320);
+			spriteBatch.end();
+		} else {
+			whiteout -= delta;
+			spriteBatch.begin();
+			spriteBatch.setColor(0.9f, 0.1f, 0.1f, 1.0f-(whiteout/WHITEOUT_TIME));
+			spriteBatch.draw(whiteTex, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 0, 0, 1, 1, false, false);
+			spriteBatch.end();
+		}
+	}
+
+	private void update(float delta) {
 		if (Gdx.input.isTouched()) {
 			// 터치 이벤트는 화면 왼쪽 위가 (0, 0)
 			Vector2 touchPos = new Vector2(Gdx.input.getX(), viewport.screenHeight - Gdx.input.getY());
@@ -88,17 +118,6 @@ public class GameScreen implements Screen {
 		player.update(delta);
 		world.update(delta, player);
 		viewport.focusOn(player.position);
-		
-		Gdx.gl10.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		world.render(viewport);
-		player.render(viewport);
-		inventoryView.render();
-		if (craftDialog.shown)
-			craftDialog.render();
-		
-		spriteBatch.begin();
-		font.draw(spriteBatch, String.format("Day %d - %s", Clock.getDay(), Clock.isNight()? "Night" : "Day"), 10, 320);
-		spriteBatch.end();
 	}
 
 	@Override
@@ -109,6 +128,7 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void show() {
+		GameState.gameOver = false;
 		spriteBatch = new SpriteBatch();
 		world = new World();
 		player = new Player(world);
@@ -120,6 +140,8 @@ public class GameScreen implements Screen {
 		viewport.focusOn(player.position);
 		ItemDB.loadTextures();
 		Clock.reset();
+		whiteout = WHITEOUT_TIME;
+		whiteTex = new Texture("data/1x1.png");
 	}
 
 	@Override
