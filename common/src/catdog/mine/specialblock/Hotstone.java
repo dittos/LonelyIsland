63aa0a79e06920b4f33c35c1c51020cb1577897f
sique <sique@localhost>
2011-06-07 12:49:39 +0000

    Hotstone

---
+++ common/src/catdog/mine/specialblock/Hotstone.java
@@ -0,0 +1,20 @@
+package catdog.mine.specialblock;
+
+import catdog.mine.Item.Interaction;
+import catdog.mine.monster.Mob;
+
+public class Hotstone implements Interaction{
+
+	private float damage = 0.3f;
+	
+	@Override
+	public void boundIn(Mob mob, int blockX, int blockY, float delta) {
+	mob.hit(damage);
+	}
+	
+	@Override
+	public void boundOut(Mob mob, int blockX, int blockY, float delta) {
+		;
+	}
+	
+}
