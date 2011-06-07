c93f3d9cc88c381bebd7fc7bf02f17579e88ed3c
Taeho Kim <dittos@gmail.com>
2011-06-07 19:21:35 +0000

    r116으로 롤백

--- common/src/catdog/mine/specialblock/Electower.java
+++ common/src/catdog/mine/specialblock/Electower.java
@@ -8,23 +8,15 @@ import catdog.mine.monster.Mob;
 
 public class Electower implements Interaction{
 
-	private static final float DAMAGE = 0.5f;
+	private float damage = 0.5f;
 	
 	@Override
 	public void boundIn(Mob mob, Collection<Mob> allMobs, Block targetBlock, int blockX, int blockY, float delta) {
-		int mobs = 0, range = targetBlock.getItem().getInteractDist();
-		
-		for(Mob other: allMobs)
-		{
-			if(Math.abs(other.position.x - blockX) + Math.abs(other.position.y - blockY) <= range)
-				++ mobs;
-		}
-		
-		mob.hit(DAMAGE * delta / mobs);
+	mob.hit(damage*delta);
 	}
-	
 	@Override
 	public void boundOut(Mob mob, Collection<Mob> allMobs, Block targetBlock, int blockX, int blockY, float delta) {
+		;
 	}
 	
 }
